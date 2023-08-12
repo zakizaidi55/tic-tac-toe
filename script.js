ecb8fc2f4af499ea62a244b7710bfd77423738b8
zakizaidi55 <zakizaidi55@gmail.com>
2023-08-12 17:20:51 +0530

    initial function in javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winnigPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+// let create a function to iitialise the game
+function initGame() {
+    currentPlayer = "X";
+    gameGrid = ["", "", "", "", "", "", "", "", ""];
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current player - ${currentPlayer}` 
+}
+
+initGame();
